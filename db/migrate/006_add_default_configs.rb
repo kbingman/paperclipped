cbf6958ef29fb494d7ed7cb4972005455793c629
Brian Landau <brianjlandau@gmail.com>
2008-10-09 05:49:41 +0800

    Allow for Settings Extension.
    Signed-off-by: Keith BIngman <keith@keithbingman.com>

--- db/migrate/006_add_default_configs.rb
+++ db/migrate/006_add_default_configs.rb
@@ -6,16 +6,18 @@ class AddDefaultConfigs < ActiveRecord::Migration
     Radiant::Config['assets.additional_thumbnails'] = "normal=640x640>"
     Radiant::Config['assets.display_size'] = "original"
     puts "-- Setting default display sizes in Radiant::Config"
-    # Config.find(:all).each do |c|
-    #  description = case c.key
-    #    when 'assets.additional_thumbnails'
-    #      'Defines the default sizes for image assets that are created when an image is uploaded. Use "#" to crop the image to a specific size. "42x42#" would be a square thumbnail, cropped in the center 42 pixels by 42 pixels.'
-    #    
-    #    when 'assets.display_size'
-    #      'Sets which of your image sizes is shown is the edit view. Defaults to the "original" image size, but any size may be used. '
-    #  end
-    #  c.update_attribute :description, description
-    # end
+    if defined? SettingsExtension
+      Config.find(:all).each do |c|
+       description = case c.key
+         when 'assets.additional_thumbnails'
+           'Defines the default sizes for image assets that are created when an image is uploaded. Use "#" to crop the image to a specific size. "42x42#" would be a square thumbnail, cropped in the center 42 pixels by 42 pixels.'
+       
+         when 'assets.display_size'
+           'Sets which of your image sizes is shown is the edit view. Defaults to the "original" image size, but any size may be used. '
+       end
+       c.update_attribute :description, description
+      end
+    end
   end
 
   def self.down
