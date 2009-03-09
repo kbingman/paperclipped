59274c2e0bd5919df03a5fbb278b5a6346fede90
Istvan Hoka <istvan.hoka@gmail.com>
2009-03-09 11:51:00 +0200

    handle the case when page_attachments have title and description fields

--- app/models/old_page_attachment.rb
+++ app/models/old_page_attachment.rb
@@ -1,11 +1,16 @@
 class OldPageAttachment < ActiveRecord::Base
     def create_paperclipped_record
-      a = Asset.new(
+      options = {
         :asset_file_size => size,
         :asset_file_name => filename,
         :asset_content_type => content_type,
         :created_by_id => created_by
-        )
+      }
+      
+      options[:title] = title if respond_to?(:title)
+      options[:caption] = description if respond_to?(:description)
+      
+      a = Asset.new(options)
       a.save
       # re-attach the asset to it's page
       page = Page.find(page_id)
