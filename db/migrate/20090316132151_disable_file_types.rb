ded568b82f219799da98f826f45cdda6a51387fc
Michael Kessler <michi@netzpiraten.ch>
2009-03-17 04:34:33 +0800

    Fix migration by prefixing the Config module with Radiant
    Signed-off-by: Keith BIngman <keith@keithbingman.com>

--- db/migrate/20090316132151_disable_file_types.rb
+++ db/migrate/20090316132151_disable_file_types.rb
@@ -3,7 +3,7 @@ class DisableFileTypes < ActiveRecord::Migration
     Radiant::Config['assets.skip_filetype_validation'] = true
     puts "-- Setting default content types in Radiant::Config"
     if defined? SettingsExtension && Radiant::Config.column_names.include?('description')
-      Config.find(:all).each do |c|
+      Radiant::Config.find(:all).each do |c|
        description = case c.key
          when 'assets.skip_filetype_validations'
            'When set to true, disables the filetype validations. Set to false to enable them.'
