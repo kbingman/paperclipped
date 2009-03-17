bca61db450eca9692dc5b16ee46b870b80475533
will <will@spanner.org>
2009-03-17 12:19:33 +0000

    drop works on new page parts

--- public/javascripts/admin/assets.js
+++ public/javascripts/admin/assets.js
@@ -1,32 +1,7 @@
 document.observe("dom:loaded", function() {
-  $$('.textarea').each(function(box){
-    Droppables.add(box, {
-      accept: 'asset',
-      onDrop: function(element) {
-        var link = element.select('a.bucket_link')[0];
-        var asset_id = element.id.split('_').last();
-        var classes = element.className.split(' ');
-        var tag_type = classes[0];
-        var tag = '<r:assets:' + tag_type + ' id="' + asset_id + '" size="original" />';
-        //Form.Element.focus(box);
-      	if(!!document.selection){
-      		box.focus();
-      		var range = (box.range) ? box.range : document.selection.createRange();
-      		range.text = tag;
-      		range.select();
-      	}else if(!!box.setSelectionRange){
-      		var selection_start = box.selectionStart;
-      		box.value = box.value.substring(0,selection_start) + tag + box.value.substring(box.selectionEnd);
-      		box.setSelectionRange(selection_start + tag.length,selection_start + tag.length);
-      	}
-      	box.focus();
-      }
-    });
-  });      
   if($('#asset-bucket')){
     new Draggable('asset-bucket', { starteffect: false, endeffect: false });
   }
-  
 });
 
 var Asset = {};
@@ -57,12 +32,45 @@ Asset.ChooseTabByName = function (tabname) {
   Asset.ChooseTab(element);
 }
 
+// factored out so that it can be called after new page part creation
+
+Asset.MakeDroppables = function () {
+  $$('.textarea').each(function(box){
+    if (!box.hasClassName('droppable')) {
+      Droppables.add(box, {
+        accept: 'asset',
+        onDrop: function(element) {
+          var link = element.select('a.bucket_link')[0];
+          var asset_id = element.id.split('_').last();
+          var classes = element.className.split(' ');
+          var tag_type = classes[0];
+          var tag = '<r:assets:' + tag_type + ' id="' + asset_id + '" size="original" />';
+          //Form.Element.focus(box);
+        	if(!!document.selection){
+        		box.focus();
+        		var range = (box.range) ? box.range : document.selection.createRange();
+        		range.text = tag;
+        		range.select();
+        	}else if(!!box.setSelectionRange){
+        		var selection_start = box.selectionStart;
+        		box.value = box.value.substring(0,selection_start) + tag + box.value.substring(box.selectionEnd);
+        		box.setSelectionRange(selection_start + tag.length,selection_start + tag.length);
+        	}
+        	box.focus();
+        }
+      });      
+    	box.addClassName('droppable');
+    }
+  });
+}
+
 Asset.ShowBucket = Behavior.create({
   onclick: function(e){
     e.stop();
     var element = $('asset-bucket');
     center(element);
     element.show();
+    Asset.MakeDroppables();
   }
 });
 
@@ -128,6 +136,3 @@ Event.addBehavior({
   '#filesearchform a' : Asset.FileTypes,
   '#asset-upload'     : Asset.WaitingForm
 });
-
-
-
