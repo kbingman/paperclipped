b7ab8fa9d207caa25e5f59cab78601f1da32db6a
Keith Bingman <keith@Gandalf-2.local>
2009-03-25 15:02:15 +0100

    fixed minor js bug in dragging the assets window. Made the link toggle the window

--- public/javascripts/admin/assets.js
+++ public/javascripts/admin/assets.js
@@ -1,5 +1,5 @@
 document.observe("dom:loaded", function() {
-  if($('#asset-bucket').length > 0){
+  if($('asset-bucket')){
     new Draggable('asset-bucket', { starteffect: false, endeffect: false });
   }
 });
@@ -69,7 +69,7 @@ Asset.ShowBucket = Behavior.create({
     e.stop();
     var element = $('asset-bucket');
     center(element);
-    element.show();
+    element.toggle();
     Asset.MakeDroppables();
   }
 });
