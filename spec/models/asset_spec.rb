kbingman/paperclipped

commit cd2c1dc173dcc3fd9dd10e2036585c14bf9f129d

cleaning up model, starting to move js out of views

app/models/asset.rb
@@ -1,22 +1,4 @@
 class Asset < ActiveRecord::Base
-  # used for extra mime types that dont follow the convention
-  @@image_content_types = ['image/jpeg', 'image/pjpeg', 'image/gif', 'image/png', 'image/x-png', 'image/jpg']
-  @@extra_content_types = { :audio => ['application/ogg'], :movie => ['application/x-shockwave-flash'], :pdf => ['application/pdf'] }.freeze
-  cattr_reader :extra_content_types, :image_content_types
-
-  # use #send due to a ruby 1.8.2 issue
-  @@image_condition = send(:sanitize_sql, ['asset_content_type IN (?)', image_content_types]).freeze
-  @@movie_condition = send(:sanitize_sql, ['asset_content_type LIKE ? OR asset_content_type IN (?)', 'video%', extra_content_types[:movie]]).freeze
-  @@audio_condition = send(:sanitize_sql, ['asset_content_type LIKE ? OR asset_content_type IN (?)', 'audio%', extra_content_types[:audio]]).freeze
-  
-  @@other_condition = send(:sanitize_sql, [
-    'asset_content_type NOT LIKE ? AND asset_content_type NOT LIKE ? AND asset_content_type NOT IN (?)',
-    'audio%', 'video%', (extra_content_types[:movie] + extra_content_types[:audio] + image_content_types)]).freeze
-  cattr_reader *%w(movie audio image other).collect! { |t| "#{t}_condition".to_sym }
-  
-  %w(movie audio image other).each do |type|
-    named_scope type.pluralize.intern, :conditions => self.send("#{type}_condition".intern)
-  end
   
   class << self
     def image?(asset_content_type)
@@ -50,10 +32,10 @@ class Asset < ActiveRecord::Base
     def types_to_conditions(types)
       types.collect! { |t| '(' + send("#{t}_condition") + ')' }
     end
-            
+    
     def thumbnail_sizes
       if Radiant::Config.table_exists? && Radiant::Config["assets.additional_thumbnails"]
-        thumbnails = Radiant::Config["assets.additional_thumbnails"].split(', ').collect{|s| s.split('=')}.inject({}) {|ha, (k, v)| ha[k.to_sym] = v; ha}
+        thumbnails = additional_thumbnails
       else
         thumbnails = {}
       end
@@ -61,19 +43,23 @@ class Asset < ActiveRecord::Base
       thumbnails[:thumbnail] = ['100x100>', :png]
       thumbnails
     end
-    
+
     def thumbnail_names
       thumbnail_sizes.keys
     end
+    
+    private
+      def additional_thumbnails
+        Radiant::Config["assets.additional_thumbnails"].gsub(' ','').split(',').collect{|s| s.split('=')}.inject({}) {|ha, (k, v)| ha[k.to_sym] = v; ha}
+      end
   end
   
-  
-  order_by 'title'
+  # order_by 'title'
     
   has_attached_file :asset,
                     :styles => thumbnail_sizes,
                     :whiny_thumbnails => false,
-                    :storage => Radiant::Config["assets.storage"] == "s3" ? :s3 : :filesystem, 
+                    :storage => Radiant::Config["assets.storage"].downcase == "s3" ? :s3 : :filesystem, 
                     :s3_credentials => {
                       :access_key_id => Radiant::Config["assets.s3.key"],
                       :secret_access_key => Radiant::Config["assets.s3.secret"]
@@ -90,27 +76,20 @@ class Asset < ActiveRecord::Base
   
   validates_attachment_presence :asset, :message => "You must choose a file to upload!"
   validates_attachment_content_type :asset, 
-    :content_type => Radiant::Config["assets.content_types"].split(', ') if Radiant::Config.table_exists? && Radiant::Config["assets.content_types"] && Radiant::Config["assets.skip_filetype_validation"] == nil
+    :content_type => Radiant::Config["assets.content_types"].gsub(' ','').split(',') if Radiant::Config.table_exists? && Radiant::Config["assets.content_types"] && Radiant::Config["assets.skip_filetype_validation"] == nil
   validates_attachment_size :asset, 
     :less_than => Radiant::Config["assets.max_asset_size"].to_i.megabytes if Radiant::Config.table_exists? && Radiant::Config["assets.max_asset_size"]
     
   before_save :assign_title
     
-  def thumbnail(size = nil)
-    if size == 'original' or size.nil?
-      self.asset.url
+  def thumbnail(size='original')
+    case 
+      when self.pdf?   : "/images/assets/pdf_#{size.to_s}.png"
+      when self.movie? : "/images/assets/movie_#{size.to_s}.png"
+      when self.audio? : "/images/assets/audio_#{size.to_s}.png"
+      when self.other? : "/images/assets/doc_#{size.to_s}.png"
     else
-      if self.pdf?
-        "/images/assets/pdf_#{size.to_s}.png"
-      elsif self.movie?
-        "/images/assets/movie_#{size.to_s}.png"
-      elsif self.audio?
-        "/images/assets/audio_#{size.to_s}.png"
-      elsif self.other?
-        "/images/assets/doc_#{size.to_s}.png"
-      else
-        self.asset.url(size.to_sym)
-      end
+      self.asset.url(size.to_sym)
     end
   end
   
@@ -118,10 +97,8 @@ class Asset < ActiveRecord::Base
     size = args[:size] 
     format = args[:format] || :jpg
     asset = self.asset
-    if asset.exists?(name.to_sym)
-      return false
-    else
-      self.asset.styles[name.to_sym] = {:geometry => size, :format => format, :whiny => true, :convert_options=>"", :processors=>[:thumbnail]} 
+    unless asset.exists?(name.to_sym)
+      self.asset.styles[name.to_sym] = { :geometry => size, :format => format, :whiny => true, :convert_options => "", :processors => [:thumbnail] } 
       self.asset.reprocess!
     end
   end
@@ -141,7 +118,7 @@ class Asset < ActiveRecord::Base
   def dimensions(size='original')
     @dimensions ||= {}
     @dimensions[size] ||= image? && begin
-      image_file = "#{RAILS_ROOT}/public#{thumbnail(size)}"
+      image_file = self.path(size)
       image_size = ImageSize.new(open(image_file).read)
       [image_size.get_width, image_size.get_height]
     rescue
@@ -191,13 +168,31 @@ class Asset < ActiveRecord::Base
     @count_by_conditions ||= @conditions.empty? ? Asset.count(:all, :conditions => type_conditions) : Asset.count(:all, :conditions => @conditions)
   end  
   
+  # used for extra mime types that do not follow the convention
+  @@image_content_types = ['image/jpeg', 'image/pjpeg', 'image/gif', 'image/png', 'image/x-png', 'image/jpg']
+  @@extra_content_types = { :audio => ['application/ogg'], 
+                            :movie => ['application/x-shockwave-flash'], 
+                            :pdf => ['application/pdf'] }.freeze
+  cattr_reader :extra_content_types, :image_content_types
+
+  # use #send due to a ruby 1.8.2 issue
+  @@image_condition = send(:sanitize_sql, ['asset_content_type IN (?)', image_content_types]).freeze
+  @@movie_condition = send(:sanitize_sql, ['asset_content_type LIKE ? OR asset_content_type IN (?)', 'video%', extra_content_types[:movie]]).freeze
+  @@audio_condition = send(:sanitize_sql, ['asset_content_type LIKE ? OR asset_content_type IN (?)', 'audio%', extra_content_types[:audio]]).freeze
+  
+  @@other_condition = send(:sanitize_sql, [
+    'asset_content_type NOT LIKE ? AND asset_content_type NOT LIKE ? AND asset_content_type NOT IN (?)',
+    'audio%', 'video%', (extra_content_types[:movie] + extra_content_types[:audio] + image_content_types)]).freeze
+  cattr_reader *%w(movie audio image other).collect! { |t| "#{t}_condition".to_sym }
+  
+  %w(movie audio image other).each do |type|
+    named_scope type.pluralize.intern, :conditions => self.send("#{type}_condition".intern)
+  end
+  
   private
   
     def assign_title
       self.title = basename if title.blank?
     end
     
-    def additional_thumbnails
-      Radiant::Config["assets.additional_thumbnails"].split(',').collect{|s| s.split('=')}.inject({}) {|ha, (k.to_sym, v)| ha[k] = v; ha}
-    end
 end

app/controllers/assets_controller.rb
@@ -9,10 +9,24 @@ class AssetsController < ApplicationController
           @asset_page = Page.find(params[:asset_page])
           render :partial => 'assets/search_results.html.haml', :layout => false
         else
-          render :partial => 'assets/asset_table.html.haml', :layout => false
+          render :partial => 'assets/asset_table.html.haml', :locals => { :assets => @assets }, :layout => false
         end
       }
     end
+    
+    before :index do
+      @template_name = 'index'
+      if params[:asset_page]
+        @page = Page.find(params[:asset_page])
+      end
+    end
+    before :edit do
+      @template_name = 'edit'
+    end
+    before :new do
+      @template_name = 'edit'
+    end
+    
     after :create do
       if params[:page]
         @page = Page.find(params[:page])
@@ -25,9 +39,9 @@ class AssetsController < ApplicationController
     end
     
     after :update do
-      ResponseCache.instance.clear
+      clear_model_cache
     end
-    
+        
     response_for :update do |format|
       format.html { 
         flash[:notice] = "Asset successfully updated."
@@ -44,7 +58,7 @@ class AssetsController < ApplicationController
           render :update do |page|
             page.call('Asset.ChooseTabByName', 'page-attachments')
             page.insert_html :bottom, "attachments", :partial => 'assets/asset', :object => @asset, :locals => {:dom_id => "attachment_#{@asset.id}" }    # can i be bothered to find the attachment id?
-            page.call('Asset.AddAsset', "attachment_#{@asset.id}")          # we ought to reinitialise the sortable attachments too
+            page.call('Asset.AddAsset', "attachment_#{@asset.id}")  # we ought to reinitialise the sortable attachments too
             page.visual_effect :highlight, "attachment_#{@asset.id}" 
             page.call('Asset.ResetForm')
           end
@@ -117,10 +131,11 @@ class AssetsController < ApplicationController
     @asset = Asset.find(params[:asset])
     @page = Page.find(params[:page])
     @page.assets << @asset unless @page.assets.include?(@asset)
-    ResponseCache.instance.clear
-    render :update do |page|
-      page[:attachments].replace_html "#{render :partial => 'page_assets', :locals => {:page => @page}}"
-    end
+    clear_model_cache
+    render :partial => 'page_assets', :locals => { :page => @page }
+    # render :update do |page|
+    #   page[:attachments].replace_html "#{render :partial => 'page_assets', :locals => {:page => @page}}"
+    # end
     
   end
   
@@ -128,7 +143,7 @@ class AssetsController < ApplicationController
     @asset = Asset.find(params[:asset])
     @page = Page.find(params[:page])
     @page.assets.delete(@asset)
-    ResponseCache.instance.clear
+    clear_model_cache
     render :nothing => true
   end
   
@@ -138,17 +153,16 @@ class AssetsController < ApplicationController
       page_attachment.position = idx+1
       page_attachment.save
     end
-    ResponseCache.instance.clear
+    clear_model_cache
     render :nothing => true
   end
   
   def remove 
     @asset = Asset.find(params[:id])
-    # This is a temporary measure! It is not very RESTful, but I like the confirm page... 
     if request.post?
       session[:bucket].delete(@asset.asset.url) if session[:bucket] && session[:bucket].key?(@asset.asset.url)
       @asset.destroy
-      ResponseCache.instance.clear
+      clear_model_cache
       redirect_to assets_path
     end 
   end
@@ -156,7 +170,11 @@ class AssetsController < ApplicationController
   protected
   
     def current_objects
-      Asset.search(params['search'], params['filter'], params['page'])
+      Asset.search(params[:search], params[:filter], params[:page])
+    end
+    
+    def clear_model_cache
+      Radiant::Cache.clear if defined?(Radiant::Cache)
     end
 
 end

app/models/page_asset_associations.rb
@@ -1,4 +1,5 @@
 module PageAssetAssociations
+  
   def self.included(base)
     base.class_eval {
       has_many :page_attachments, :order => :position

app/views/assets/_asset.html.haml
@@ -4,9 +4,9 @@
     = link_to image_tag(asset.thumbnail(:thumbnail)), asset.thumbnail, :onclick => "return false;", :class => "bucket_link", :title => asset.title
   .info{ :id => "#{dom_id}_info" }
     %strong
-      = link_to "edit", edit_asset_url(asset.id), :id => "edit_asset", :title => "Edit #{asset.title}"
+      = link_to image_tag('/images/assets/edit.png'), edit_asset_url(asset.id), :class => "edit_asset", :title => "Edit #{asset.title}"
     - if @page
-      = link_to_remote "remove", :html => { :id => "remove_asset", :title => "Remove from page" }, :url => remove_page_asset_url(:asset => asset.id, :page => @page.id), | 
+      = link_to_remote image_tag('/images/assets/delete.png'), :html => { :class => "remove_asset", :title => "Remove from page" }, :url => remove_page_asset_url(:asset => asset.id, :page => @page.id), | 
         :confirm => "Are you sure you want to remove this asset from the page?", |
         :after => "Effect.Fade('#{dom_id}', { duration: 0.5 })", :complete => "Element.remove('#{dom_id}'); Element.highlight('assets')" |
       = image_tag "/images/assets/reorder_assets.png", :class => "reorder", :title => "Reorder"

app/views/assets/_asset_table.html.haml
@@ -1,37 +1,44 @@
 %table{ :class => "index", :cellpadding => "0", :cellspacing => "0", :border => "0"}
   %thead
     %tr
-      %th.asset Image
-      %th.asset-title Title
-      %th.content_type Type
-      %th.modify{ :colspan =>"2"} Modify
+      - render_region :thead do |thead|
+        - thead.thumbnail_header do
+          %th.asset Asset
+        - thead.title_header do
+          %th.asset-title Title
+        - thead.content_type_header do
+          %th.content_type Type
+        - thead.modify_header do
+          %th.modify{ :colspan =>"2"} Modify
 
   %tbody
-    - unless @assets.empty?
-      - for asset in @assets 
+    - unless assets.empty?
+      - for asset in assets 
         %tr.node.level-1
-          %td.asset
-            - asset_id = "asset_#{asset.id.to_s}"
-            = link_to image_tag(asset.thumbnail(:icon)), edit_asset_url(:id => asset), :class => 'icon'
-          %td.asset-title
-            = link_to asset.title, edit_asset_url(:id => asset.id) 
-            &nbsp;
-            %span.caption
-              = asset.caption
-          %td.content_type
-            = asset.asset_content_type
-          %td.add-to-bucket
-            = link_to_remote(image_tag('assets/add-to-bucket.png', :alt => 'Add to Bucket'), :url => add_bucket_url(:id => asset))
-          %td.remove
-            = link_to image_tag('admin/remove.png', :alt => 'Remove Asset'), remove_asset_url(:id => asset)
+          - render_region :tbody, :locals => { :asset => asset } do |tbody|
+            - tbody.thumbnail_cell do
+              %td.asset
+                - asset_id = "asset_#{asset.id.to_s}"
+                = link_to image_tag(asset.thumbnail(:icon)), edit_asset_url(:id => asset), :class => 'icon'
+            - tbody.title_cell do
+              %td.asset-title
+                = link_to asset.title, edit_asset_url(:id => asset.id) 
+                &nbsp;
+                %span.caption
+                  = asset.caption
+            - tbody.content_type_cell do
+              %td.content_type
+                = asset.asset_content_type
+            - tbody.bucket_cell do
+              %td.add-to-bucket
+                = link_to_remote(image_tag('assets/add-to-bucket.png', :alt => 'Add to Bucket'), :url => add_bucket_url(:id => asset))
+            - tbody.remove_cell do
+              %td.remove
+                = link_to image_tag('admin/remove.png', :alt => 'Remove Asset'), remove_asset_url(:id => asset)
     - else
       %tr
         %td.note{ :colspan => "5" } 
           No Assets
-    
+
 = will_paginate @assets
 
-%script{ :type => "text/javascript"}
-  // <! [CDATA[
-  new RuledTable('assets')
-  //]

app/views/assets/_assets_container.html.haml
@@ -3,44 +3,66 @@
 - include_javascript 'admin/assets'
 - include_javascript 'dragdrop'
 
-#asset-tabs
-  - if @page && !@page.new_record? 
-    %a.asset-tab.here{ :href => "#bucket", :id => "tab_bucket" } Bucket
-    %a.asset-tab{ :href => "#page-attachments", :id => "tab_page-attachments" } Attached Assets
-    %a.asset-tab{ :href => "#upload-assets", :id => "tab_upload-assets" } Upload
-    // %a.asset-tab{ :href => "#search-assets" } Search
-  - else
-    %a.asset-tab.here{ :href => "#" } Bucket
+#assets-container
+  #asset-tabs
+    - render_region :asset_tabs do |asset_tabs|
+      - if @page && !@page.new_record? 
+        - asset_tabs.attachment_tab do
+          %a.asset-tab{ :href => "#page-attachments", :id => "tab_page-attachments" } Attached Assets
+        - asset_tabs.upload_tab do
+          %a.asset-tab{ :href => "#upload-assets", :id => "tab_upload-assets" } Upload
+        - asset_tabs.bucket_tab do
+          %a.asset-tab{ :href => "#bucket", :id => "tab_bucket" } Bucket
+        - asset_tabs.search_tab do  
+          %a.asset-tab{ :href => "#search-assets" } Search
+      - else
+        - asset_tabs.bucket_tab do
+          %a.asset-tab.here{ :href => "#" } Bucket
 
-#assets      
-  #bucket.pane
-    %p
-      %span.note Items here can be inserted by simply dragging them to the text area. 
-      %span#clear-bucket
-        = link_to_remote('Clear Bucket', {:url => clear_bucket_url} )
+  #assets 
+    #bucket.pane
+      - render_region :bucket_pane do |bucket_pane|
+        - bucket_pane.bucket_notes do      
+          %p
+            %span.note The Bucket provides temporary storage for your items. You can use it to place items in your pages or snippets.
+            %span#clear-bucket
+              = link_to_remote('Clear Bucket', {:url => clear_bucket_url} )
+        - bucket_pane.bucket do 
+          = render :partial => "assets/bucket"
+        - bucket_pane.bucket_bottom do 
+          .clear
 
-    = render :partial => "assets/bucket"
-    %div{ :style => "clear:both;" }
+      
+    - if @page && !@page.new_record? 
+      - render_region :asset_panes do |asset_panes|
 
-  - if @page && !@page.new_record? 
-    #page-attachments.pane{ :style => "display:none;" }
-      %p
-        %span.note Attached assets. These can be inserted with drag and drop or shown with the <code>&lt;r:assets:each /&gt;</code> tag.
-      %ul#attachments
-        = render :partial => "assets/page_assets" 
-  
-      %div{ :style => "clear:both;" }
-    #upload-assets.pane{ :style => "display:none;" }
-      %p
-        %span.note Assets uploaded here will be automatically associated with this page.
-      = render :partial => "assets/upload_to_page"
-    #search-assets.pane{ :style => "display:none;" }
-      %p
-        %span.note Search for assets 
-        %input{ :type => "text", :id => "search", :name => "search" }
-    
-      #search-results
-      = observe_field 'search',  :frequency => 1, :update => 'search-results', :url =>  assets_path(:asset_page => @page), :method => 'get', :with => "'search=' + escape(value)"
+        - asset_panes.page_attachments do 
+          #page-attachments.pane
+            %p
+              %span.note 
+                Attached assets. These can be inserted with drag and drop 
+                or shown with the <code>&lt;r:assets:each /&gt;</code> tag.
+            %ul#attachments
+              = render :partial => "assets/page_assets" 
+            .clear
+      
+        - asset_panes.upload do
+          #upload-assets.pane
+            %p
+              %span.note Assets uploaded here will be automatically associated with this page.
+            = render :partial => "assets/upload_to_page"
+            .clear
+          
+        - asset_panes.search do
+          #search-assets.pane
+            %p
+              %label.note{ :for => 'search' } Search for assets 
+              %input{ :type => "hidden", :name => "search", :value => @page.id }
+              %input{ :type => "search", :id => "search", :name => "search" }
+              = submit_tag "Search"
+            #search-results
+            .clear
+            = observe_field 'search',  :frequency => 1, :update => 'search-results', :url =>  assets_path(:asset_page => @page), :method => 'get', :with => "'search=' + escape(value)"
 
 
     

app/views/assets/_bucket.html.haml
@@ -1,20 +1,10 @@
 %ul#bucket_list
   - if session[:bucket]
     - session[:bucket].each do |url, info| 
-      - dom_id = "bucket_#{info[:id]}"
-      %li
-        - asset_type = info[:type]
-        %div{ :class => "#{asset_type} asset", :id => dom_id }
-          = link_to image_tag(info[:thumbnail]), url, :onclick => "return false;", :title => info[:title], :class => "bucket_link"
-        .info{ :id => "#{dom_id}_info" }
-          = link_to "edit", edit_asset_url(info[:id]), :id => "edit_asset", :title => "Edit #{info[:title]}"
-          - if @page && !@page.new_record?
-            
-            = link_to_remote "add", :html => { :id => "add_asset", :title => "Attach to page" }, :url => attach_page_asset_url(:asset => info[:id], :page => @page.id), |
-                                             :after => "Element.highlight('assets');" |
-
+      = render :partial => 'assets/bucket_asset', :locals => { :asset_url => url, :asset_id => info[:id], :asset_type => info[:type], :asset_title => info[:title], :asset_thumbnail =>info[:thumbnail], :page => @page }
   - else
     %li
       %p.note Your bucket is empty
       
+ 
 

app/views/assets/_bucket_asset.html.haml
@@ -1,0 +1,9 @@
+%li
+
+  %div{ :class => "#{asset_type} asset", :id =>  "bucket_#{asset_id}" }
+    = link_to image_tag(asset_thumbnail), asset_url, :title => asset_title, :class => "bucket_link"
+
+  .info{ :id => "#{asset_id}_info" }
+    = link_to image_tag('/images/assets/edit.png'), edit_asset_url(asset_id), :class => "edit_asset", :title => "Edit #{asset_title}"
+    - if page && !page.new_record?
+      = link_to image_tag('/images/assets/add.png'), attach_page_asset_url(:asset => asset_id, :page => page.id), :class => "add_asset", :title => "Attach to page"

commit 5daa84dc5c23c2730e296d611440f77a3411fb8e

fix for #thumbnail method not working as expected for non-image assets
* bug was introduced in cd2c1dc
* reverted back to behaviour from 319b8365
* added some basic specs to cover the method

app/models/asset.rb
@@ -83,6 +83,7 @@ class Asset < ActiveRecord::Base
   before_save :assign_title
     
   def thumbnail(size='original')
+    return asset.url if size == 'original'
     case 
       when self.pdf?   : "/images/assets/pdf_#{size.to_s}.png"
       when self.movie? : "/images/assets/movie_#{size.to_s}.png"

spec/models/asset_spec.rb
@@ -1,11 +1,68 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Asset do
-  before(:each) do
-    @asset = Asset.make
+  def default_attributes
+    {
+      :asset_file_name =>  'asset.jpg',
+      :asset_content_type =>  'image/jpeg',
+      :asset_file_size => '46248'
+    }
+  end
+  def new_asset(overrides={})
+    Asset.new default_attributes.merge(overrides)
+  end
+  def create_asset(overrides={})
+    Asset.create! default_attributes.merge(overrides)
+  end
+  
+  it 'should be valid when instantiated' do
+    new_asset.should be_valid
+  end
+  
+  it 'should be valid when saved' do
+    create_asset.should be_valid
+  end
+  
+  describe '#thumbnail' do
+    describe 'without argument' do
+      it 'should return paperclip asset url for image' do
+        image = new_asset :asset_content_type => 'image/jpeg'
+        image.stub! :asset => mock('asset', :url => '/y/z/e.jpg')
+        image.thumbnail.should == '/y/z/e.jpg'
+      end
+      
+      it 'should return paperclip asset url for non-image' do
+        asset = new_asset :asset_content_type => 'application/pdf'
+        asset.stub! :asset => mock('asset', :url => '/y/z/e.pdf')
+        asset.thumbnail.should == '/y/z/e.pdf'
+      end
+    end
+    
+    describe 'with size=original' do
+      it 'should return paperclip asset url for image' do
+        image = new_asset :asset_content_type => 'image/jpeg'
+        image.stub! :asset => mock('asset', :url => '/y/z/e.jpg')
+        image.thumbnail('original').should == '/y/z/e.jpg'
+      end
+      
+      it 'should return paperclip asset url for non-image' do
+        asset = new_asset :asset_content_type => 'application/pdf'
+        asset.stub! :asset => mock('asset', :url => '/y/z/e.pdf')
+        asset.thumbnail('original').should == '/y/z/e.pdf'
+      end
+    end
+    
+    it 'should return resized image for images when given size' do
+      image = new_asset :asset_content_type => 'image/jpeg'
+      image.stub! :asset => mock('asset')
+      image.asset.stub!(:url).with(:thumbnail).and_return('/re/sized/image_thumbnail.jpg')
+      image.thumbnail('thumbnail').should == '/re/sized/image_thumbnail.jpg'
+    end
+    
+    it 'should return icon for non-image with a given size' do
+      image = new_asset :asset_content_type => 'application/pdf'
+      image.thumbnail('thumbnail').should == "/images/assets/pdf_thumbnail.png"
+    end
   end
 
-  it "should be valid" do
-    @asset.should be_valid
-  end
 end

spec/spec_helper.rb
@@ -6,10 +6,10 @@ end
 
 require 'spec/rails'
 
-require File.expand_path(File.dirname(__FILE__) + "/blueprints")
+# require File.expand_path(File.dirname(__FILE__) + "/blueprints")
 
 Spec::Runner.configure do |config|
-  # config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = true
   # config.use_instantiated_fixtures  = false
   # config.fixture_path = RAILS_ROOT + '/spec/fixtures'
 