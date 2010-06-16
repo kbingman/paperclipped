1d8c7d42e1c7cbf5a3869da7bc5b3f0a410fa004
Jim Gay <jim@saturnflyer.com>
2010-06-16 00:11:05 -0400

    default_style on the attachment, not the intstance

--- lib/url_additions.rb
+++ lib/url_additions.rb
@@ -2,7 +2,7 @@ module UrlAdditions
   
   Paperclip.interpolates :no_original_style do |attachment, style|
     style ||= :original
-    style == attachment.instance.default_style ? nil : "_#{style}"
+    style == attachment.default_style ? nil : "_#{style}"
   end
   
 end
